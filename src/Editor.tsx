52a1699b826db6efb343d53585508c32da169e53
Dmitriy Sidorenko <dmitriysidor3nk0@gmail.com>
2022-09-04 12:45:05 +0300

    cover image positioning and size

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -84,12 +84,8 @@ const CoverImage = observer(() => {
         <img
           style={{
             position: "absolute",
-            top: 0,
             left: 0,
-            right: 0,
             bottom: 0,
-            width: "100%",
-            height: "100%",
             opacity: state.opacity,
             pointerEvents: "none",
           }}
